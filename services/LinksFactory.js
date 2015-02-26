95d16fd1d3cd9c7741fd453c707bb0db427c909e
Kathryn Carr and Lizzie Koehler <kathrynelizabethcarr@gmail.com>
2015-02-26 11:43:20 -0800

    add comment section under links

--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -1,10 +1,10 @@
 nerdNews.factory("LinksFactory", function LinksFactory($location){
   var factory = {};
-  factory.links = [{description: "misophonia", address: "http://well.blogs.nytimes.com/2015/02/23/please-stop-making-that-noise/", votes: 0, id: 1},
-{description: "Synesthesia - Rare but real", address: "http://www.npr.org/programs/invisibilia/382451600/entanglement?showDate=2015-01-30", votes: 0, id: 2}];
+  factory.links = [{description: "misophonia", address: "http://well.blogs.nytimes.com/2015/02/23/please-stop-making-that-noise/", votes: 0, id: 1, comments: []},
+{description: "Synesthesia - Rare but real", address: "http://www.npr.org/programs/invisibilia/382451600/entanglement?showDate=2015-01-30", votes: 0, id: 2, comments: []}];
 
   factory.addLink = function(){
-    var link = {id: factory.links.length + 1, votes: 0, description: factory.linkDescription, address: factory.linkAddress};
+    var link = {id: factory.links.length + 1, votes: 0, description: factory.linkDescription, address: factory.linkAddress, comments: []};
     factory.links.push(link);
     factory.linkDescription = null;
     factory.linkAddress = null;
@@ -13,6 +13,11 @@ nerdNews.factory("LinksFactory", function LinksFactory($location){
 
   factory.upVote = function(id) {
     factory.links[id-1].votes += 1;
+  };
+
+  factory.addComment = function(id){
+    factory.links[id-1].comments.push(factory.linkComments);
+    factory.linkComments = null;
   };
 
   return factory;
