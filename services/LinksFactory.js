3d2b98bee04a4e456ac79908803ceea767f71af5
Kathryn Carr and Lizzie Koehler <kathrynelizabethcarr@gmail.com>
2015-02-26 09:41:39 -0800

    Add clickability to upvote image

--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -1,7 +1,7 @@
 nerdNews.factory("LinksFactory", function LinksFactory($location){
   var factory = {};
-  factory.links = [{description: "misophonia", address: "http://well.blogs.nytimes.com/2015/02/23/please-stop-making-that-noise/"},
-{description: "Synesthesia - Rare but real", address: "http://www.npr.org/programs/invisibilia/382451600/entanglement?showDate=2015-01-30"}];
+  factory.links = [{description: "misophonia", address: "http://well.blogs.nytimes.com/2015/02/23/please-stop-making-that-noise/", votes: 0, id: 1},
+{description: "Synesthesia - Rare but real", address: "http://www.npr.org/programs/invisibilia/382451600/entanglement?showDate=2015-01-30", votes: 0, id: 2}];
 
   factory.addLink = function(){
     var link = {id: factory.links.length + 1, votes: 0, description: factory.linkDescription, address: factory.linkAddress};
@@ -10,11 +10,11 @@ nerdNews.factory("LinksFactory", function LinksFactory($location){
     factory.linkAddress = null;
 
   };
-  // MAKE THIS
-  // factory.upVote = function() {
-  //
-  //
-  // };
+
+  factory.upVote = function(id) {
+debugger;
+    votes += 1;
+  };
 
   return factory;
 
