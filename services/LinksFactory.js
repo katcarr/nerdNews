ecfac9e39dc46721bc0c9127d902b8c36804d26d
Kathryn Carr and Lizzie Koehler <kathrynelizabethcarr@gmail.com>
2015-02-26 12:23:47 -0800

    Add date property to link element

--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -2,9 +2,12 @@ nerdNews.factory("LinksFactory", function LinksFactory($location){
   var factory = {};
   factory.links = [{description: "misophonia", address: "http://well.blogs.nytimes.com/2015/02/23/please-stop-making-that-noise/", votes: 0, id: 1, comments: []},
 {description: "Synesthesia - Rare but real", address: "http://www.npr.org/programs/invisibilia/382451600/entanglement?showDate=2015-01-30", votes: 0, id: 2, comments: []}];
+  factory.links[0].time = new Date();
+  factory.links[1].time = new Date();
 
   factory.addLink = function(){
     var link = {id: factory.links.length + 1, votes: 0, description: factory.linkDescription, address: factory.linkAddress, comments: []};
+    link.time = new Date();
     factory.links.push(link);
     factory.linkDescription = null;
     factory.linkAddress = null;
